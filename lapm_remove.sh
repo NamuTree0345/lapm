fc26b0127718fed536df11e384e738c4b661a530
TreeKR <46389044+NamuTree0345@users.noreply.github.com>
2020-05-14 11:06:50 +0900

    change remove to purge

--- lapm_remove.sh
+++ lapm_remove.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 echo "installer by NamuTree0345. (for debian/raspbian/ubuntu)"
 echo "tested on Raspbian"
-apt remove apache2 -y
-apt remove php php-fpm php-curl php-gd php-intl php-mbstring php-mysql php-soap php-xml php-xmlrpc php-zip libapache2-mod-php -y
-apt remove mariadb-server -y
+apt purge apache2 -y
+apt purge php php-fpm php-curl php-gd php-intl php-mbstring php-mysql php-soap php-xml php-xmlrpc php-zip libapache2-mod-php -y
+apt purge mariadb-server -y
 echo "ok."
 exit 0
