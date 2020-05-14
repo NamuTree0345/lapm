73bad334df6dd3b9b4cef58d09808e649f2057a7
TreeKR <46389044+NamuTree0345@users.noreply.github.com>
2020-05-14 10:47:31 +0900

    Update lapm_not_update.sh

--- lapm_not_update.sh
+++ lapm_not_update.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 echo "installer by NamuTree0345. (for debian/raspbian/ubuntu)"
 echo "tested on Raspbian"
+apt install apache2 -y
+apt install php php-fpm php-curl php-gd php-intl php-mbstring php-mysql php-soap php-xml php-xmlrpc php-zip libapache2-mod-php -y
+apt install mariadb-server -y
+echo "ok. please run sudo mysql_secure_installation"
 exit 0
